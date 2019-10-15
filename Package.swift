92d9cedcf727c2a5b198ae378dfd04354e4ac94f
Adam Fowler <adamfowler71@gmail.com>
2019-10-15 10:33:11 +0100

    Update to v3.3.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
-  name: "SwiftAWSApigateway",
+  name: "APIGateway",
   products: [
-      .library(name: "SwiftAWSApigateway", targets: ["SwiftAWSApigateway"]),
+      .library(name: "APIGateway", targets: ["APIGateway"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
+      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "3.4.0"))
   ],
   targets: [
-      .target(name: "SwiftAWSApigateway", dependencies: ["AWSSDKSwiftCore"]),
+      .target(name: "APIGateway", dependencies: ["AWSSDKSwiftCore"]),
   ]
 )
