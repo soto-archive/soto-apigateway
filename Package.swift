3be82f38960a47e9d11fee324345905bdaf2c56d
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 17:01:20 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "APIGateway",
-  products: [
-      .library(name: "APIGateway", targets: ["APIGateway"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "APIGateway", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "APIGateway",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "APIGateway", targets: ["APIGateway"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "APIGateway", dependencies: ["AWSSDKSwiftCore", "APIGatewayMiddleware"], path: "./Sources/APIGateway"),
+        .target(name: "APIGatewayMiddleware", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/APIGatewayMiddleware"),
+    ]
 )
